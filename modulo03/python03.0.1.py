bbb39bcbe7b03ba48718445303b1fcf6001ea0c4
Andre Luis Kunert <seuemail@example.com>
2025-08-07 11:16:29 -0300

    Bora Codar com Python3

---
+++ modulo03/python03.0.1.py
@@ -0,0 +1,6 @@
+print('Aula 2 – Para que serve o Python?')
+print("""
+De onde vem o Python? Por que esse nome?
+ Quais são as grandes empresas mundiais que usam Python em seus softwares? Essas e muitas outras perguntas serão respondidas nessa aula.
+ """)
+print('The End')
---
+++ modulo03/python03.0.3.py
@@ -0,0 +1,7 @@
+print('Aula 3 – Instalando o Python3 e o IDLE')
+print("""
+Entenda como funciona um Interpretador e veja como o Python funciona.
+ Depois aprenda como instalar o Python 3.0 e o IDLE em seu computador com Windows, Linux ou Mac OS.
+ """)
+print('Mostra como instalar Python no Windows...')
+print('The End')
