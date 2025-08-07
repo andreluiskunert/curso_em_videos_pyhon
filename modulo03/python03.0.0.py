babdb470e723e4bd09aa55582b736bd9cc534add
Andre Luis Kunert <seuemail@example.com>
2025-08-06 21:01:04 -0300

    Codando em Python..

---
+++ modulo03/python03.0.0.py
@@ -0,0 +1,3 @@
+print('Seja um Programador')
+print('Você está pensando em ser um Programador e não sabe por onde começar? Pois nessa aula vamos te dar todas as informações e mostrar o caminho a seguir.')
+print('The End')
