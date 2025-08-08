726c81836787497dbe9177dcf5052a25d84b7acb
Andre Luis Kunert <seuemail@example.com>
2025-08-08 10:40:15 -0300

    Codando com Python3

---
+++ modulo03/Exerciciios/python03.0.1.py
@@ -0,0 +1,26 @@
+print('Exercício 1 – Somando dois números')
+print("""
+Crie um programa que leia dois números e mostre a soma entre
+ """)
+# Solicita que o usuário digite dois números
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# Calcula a soma
+soma = num1 + num2
+
+# Exibe o resultado
+print("A soma entre {} e {} é {}".format(num1, num2, soma))
+
+
+# ----------------
+print('-=-=-'*15)
+print('þ Versão Guanabarra: ')
+n1 = int( input('Informe o nº01: ') )
+n2 = int(input('Informe o nº02: '))
+s = n1 + n2 
+print('A soma entre {} e {} é igual a {} .'.format(n1, n2, s))
+# ------
+
+print('-=-=-'*15)
+print('The End')
