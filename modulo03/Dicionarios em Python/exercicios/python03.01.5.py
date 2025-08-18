b2e483c35409eccd4171c5b9b34ffb9d5f76c3ab
Andre Luis Kunert <seuemail@example.com>
2025-08-18 20:31:58 -0300

    codando em Python03

--- modulo03/Dicionarios em Python/exercicios/python03.01.5.py
+++ modulo03/Dicionarios em Python/exercicios/python03.01.5.py
@@ -60,11 +60,51 @@ while True:
 
 print("<< VOLTE SEMPRE >>")
 
-
-
 print('-x-x-'*20)
 print('Versão Guanabarra: ')
 print('Bora lá pratica...')
-print('Deixar pra de noite')
+print('=<>='*25)
+time = list()
+jogador = dict()
+partidas = list()
+while True:
+    jogador['nome'] = str(input('NOme Jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou ? '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'Quantos gols na partida {c} ? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+            resp = str(input('Quer continuar? [S/N]')).upper()[0]
+            if resp in 'SN':
+                break
+            print('ERRO!Responda apenas S ou N')
+            if resp == 'N':
+                break
+print('  <>=<> '*25)
+for k, v in enumerate(time):
+    print(f'{k:>4} ' ,end='')
+    for d in v.values():
+        print(f'{str(d):<15}',end='')
+        print()
+print('cod', end='')
+for i in jogador.keys():
+    print(f'i:<15',end='')
+print(f'{str(d):<15}', end='')
+print('<>=<>'*25)
+while True: 
+    busca = int(input('Mostrar dados de qual  Jogador (999 para parar)'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO!não existe jogador com código {busca}')
+    else:
+        print(f'---Levantamento do Jogador {time[busca]["nome"]}:')
+        for i,g in enumerate(time[busca]['gols']):
+            print(f' no jogo {i} fez {g} gols')  
+    print('---=---'*25)
+    print('Volte sempre...')
 print('==>=='*20)
 print('The End')
